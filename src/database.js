2eb4c576af652d509191853631e0148f176a48a8
brittanypallasco2003 <nohemiespinel0@gmail.com>
2024-01-01 17:02:37 -0500

    Actualizar la cadena de conexiónde la base de datos

--- src/database.js
+++ src/database.js
@@ -3,14 +3,13 @@
 const mongoose = require('mongoose')
 
 //CADENAS DE CONEXION A BDD
-//const MONGODB_URI = 'mongodb+srv://byrontosh:sistemas@cluster0.6e8zntc.mongodb.net/test' -->Cadena que utiliza MONGODB ATLAS
 //const MONGODB_URI = 'mongodb://localhost:27017/portfolio'-->cadena de conexión local
 
 //CREAR UNA FUNCIÓN PARA CONECTAR LA BASE DE DATOS
 connection = async()=>{
     try {
         //invocar el método connect 
-         await mongoose.connect(process.env.MONGODB_URI)
+         await mongoose.connect(process.env.MONGODB_URI_ATLAS)
          //respuesta de la promesa
         console.log("Database is connected")
     } catch (error) {
